1029c4aab9cacd2fac87f97559f3addf0c5e1b74
RayHightower <rth@wisdomgroup.com>
2012-05-11 15:30:19 -0500

    removed Hello World. back to work!

--- Magic8Ball/app/app_delegate.rb
+++ Magic8Ball/app/app_delegate.rb
@@ -1,8 +1,5 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    alert = UIAlertView.new
-    alert.message = "Hello World!"
-    alert.show
     true
   end
 end
