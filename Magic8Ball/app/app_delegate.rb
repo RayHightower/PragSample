9f46dbfe7b3099527f343f7c58ea961491ffa50e
RayHightower <rth@wisdomgroup.com>
2012-05-11 15:41:45 -0500

    creating a flashlight, multiple colors, using UIKit

--- Magic8Ball/app/app_delegate.rb
+++ Magic8Ball/app/app_delegate.rb
@@ -1,5 +1,14 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    # Objective-C window creation, followed by the Ruby equivalent
+    # UIWindow *window = [[UIWindow alloc] initWithFrame:frame];
+    
+    # @window = UIWindow.alloc.initWithFrame(frame)
+    # 
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.backgroundColor = UIColor.blueColor
+    @window.makeKeyAndVisible  # Make window top most & visible.
+
     true
   end
 end
