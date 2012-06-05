af85f18e3664dddc81de4f5e7464165b559385be
RayHightower <rth@wisdomgroup.com>
2012-06-05 11:18:43 -0500

    random experiments for learning

--- Magic8Ball/app/app_delegate.rb
+++ Magic8Ball/app/app_delegate.rb
@@ -6,10 +6,13 @@ class AppDelegate
     # @window = UIWindow.alloc.initWithFrame(frame)
     # 
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-
     @window.rootViewController = Magic8BallViewController.alloc.init
-
     @window.makeKeyAndVisible  # Make window top most & visible.
+
+    @anotherwindow = 1 # Just a dummy var 'cause I'm testing stuff out.
+
+    # @anotherwindow = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.(0,0,20,20))
+    # @anotherwindow.makeKeyAndVisibla
 
     true
   end
