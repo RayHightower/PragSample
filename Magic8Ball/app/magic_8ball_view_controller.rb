2b7dcdd1bf4e31f0ae436706bbcfc5136390b255
Raymond T. Hightower <rth@wisdomgroup.com>
2012-08-14 12:26:56 -0500

    prepping for RubyMotion

--- Magic8Ball/app/magic_8ball_view_controller.rb
+++ Magic8Ball/app/magic_8ball_view_controller.rb
@@ -8,5 +8,9 @@ class Magic8BallViewController < UIViewController
   def viewDidLoad
     view.image = UIImage.imageNamed('background.png')
     # add background.png to resources directory  
+
+    @label = UILabel.alloc.initWithFrame([[10,60], [300,80]])
+#    view.addSubView(@label)
+
   end
 end
